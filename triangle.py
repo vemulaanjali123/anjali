0cce3554fae5afe36631f3b140f983a1e3f41f02
vemulaanjali123 <anjuvemula2006@gmail.com>
2025-04-27 23:51:01 -0700

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+#python program to print numbers in triangular
+rows=int(input("enter no of rows:"))
+for i in range (rows,0,-1):
+    print("\n")
+    for j in range(rows-i+1):
+         print(i,"",end="")
+        
+           
